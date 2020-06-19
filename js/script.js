b9d0435989ae60c48732e7aae73b58c5773c6c46
Animesh Kumar <animuz111@gmail.com>
2020-06-19 20:16:01 +0530

    New website template added
    Signed-off-by: Animesh Kumar <animuz111@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: Venu
+ * @Date:   2017-06-22 10:45:00
+ * @Last Modified by:   Venu
+ * @Last Modified time: 2017-08-27 12:13:16
+ */
+
+$(function() {
+    $("#about-me").typed({
+        strings: ["Open source enthusiast.", "Contibutor at CHAOSS, Kubernetes and Wikimedia.", "Web and mobile developer.",
+            "Data science learner.", "Distributer and containerized systems.", "Blockchain enthusiast.", "Foodie, moviebuff and wanderlust."
+        ],
+        typeSpeed: 30,
+        loop: true,
+    });
+});
